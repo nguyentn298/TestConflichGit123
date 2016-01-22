fad0d2c19cd457cb8f6235c896edbe825a64843b
nguyentn298 <kid2981@gmail.com>
2016-01-22 15:09:17 +0700

    has 1 branch

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -4,18 +4,9 @@ public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
 
-		System.out.println("Local: change 111111111");
-		System.out.println("Local: change 222222222");
-		System.out.println("Local: change 333333333");
-		System.out.println("Local: change 444444444");
 
-		System.out.println("Branch: change 111111111");
-		System.out.println("Branch: change 222222222");
-		System.out.println("Branch: change 333333333");
-		System.out.println("Branch: change 444444444");
-		System.out.println("Branch: change 444444444");
-		System.out.println("Branch: change 444444444");
-		System.out.println("Branch: change 555555555");
+		System.out.println("Branch: change 12345678");
+
 
 	}
 }
