d932d48ede6de2a6b81711f3eb67a81d9bb00007
nguyentn298 <kid2981@gmail.com>
2016-01-22 13:59:31 +0700

    test second

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -9,6 +9,7 @@ public class TestConflictGit {
 		System.out.println("test2222");
 		System.out.println("khong duoc roi");
 		System.out.println("conflict di pa");
+		System.out.println("Test tiep");
 		
 	}
 }
