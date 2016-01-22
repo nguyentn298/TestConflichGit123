bdfb075af2f5b56d055d98f708db6588770c1b92
nguyentrian85 <nguyentrian85@gmail.com>
2016-01-22 16:02:07 +0700

    test change to 987654321 and  333333

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -3,7 +3,7 @@ package test1;
 public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
-		System.out.println("Branch: change 12345678");
+		System.out.println("Branch: change 987654321");
 		System.out.println("Local: change 111111111");
 
 		System.out.println("Branch: change 555555555");
