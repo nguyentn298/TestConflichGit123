dfd971da21729e63840f6034dc54a6005ff6928a
nguyentn298 <kid2981@gmail.com>
2016-01-22 13:44:53 +0700

    Update TestConflictGit.java

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -4,5 +4,9 @@ public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
 		System.out.println("test1111");
+		System.out.println("test1111");
+		System.out.println("test1111");
+		System.out.println("test2222");
+		
 	}
 }
