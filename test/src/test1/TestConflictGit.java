cddb8e88cb3bb74e64a6444936f1e2d9fa65a410
nguyentn298 <kid2981@gmail.com>
2016-01-22 14:32:01 +0700

    Update TestConflictGit.java

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -3,10 +3,8 @@ package test1;
 public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
-
-		System.out.println("Local: change 111111111");
-
 		System.out.println("Branch: change 1");
+		System.out.println("Branch: change 2");
 
 	}
 }
