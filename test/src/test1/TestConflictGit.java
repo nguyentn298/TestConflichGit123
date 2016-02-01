08767baad42d11b149bc4ae588f7430ded27c895
nguyentn298 <kid2981@gmail.com>
2016-02-01 16:57:39 +0700

    1

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -5,5 +5,6 @@ public class TestConflictGit {
 		System.out.println("conflict github");
 		System.out.println("Branch: change 111111");
 		System.out.println("Branch: change 222222");
+		System.out.println("test");
 	}
 }
