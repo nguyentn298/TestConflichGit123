5a15b902df858193470b7469d12cad2fbd442f3c
nguyentn298 <kid2981@gmail.com>
2016-02-01 16:29:11 +0700

    2 lines

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -4,6 +4,6 @@ public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
 		System.out.println("Branch: change 111111");
-
+		System.out.println("Branch: change 222222");
 	}
 }
