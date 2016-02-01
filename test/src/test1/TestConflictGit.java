694376092274d6315c73ee1269273cc2e7b70bd1
nguyentn298 <kid2981@gmail.com>
2016-02-01 16:25:39 +0700

    1 line

--- test/src/test1/TestConflictGit.java
+++ test/src/test1/TestConflictGit.java
@@ -3,11 +3,7 @@ package test1;
 public class TestConflictGit {
 	public static void main(String[] args) {
 		System.out.println("conflict github");
-		System.out.println("Local: change 1234567");
 		System.out.println("Branch: change 111111");
-		System.out.println("Branch: change 111111");
-		System.out.println("Branch: change 222222");
-		System.out.println("Branch: change 222222");
-		System.out.println("Branch: change 333333");
+
 	}
 }
